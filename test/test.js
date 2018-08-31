675a61eeeba3a405085a6c684b291bf9f53d505f
kessler <yanivk@gmail.com>
2018-08-31 14:52:07 +0300

    upgrade debug due to CVE-2017-16137, upgrade lodash due to CVE-2018-3721

--- test/test.js
+++ test/test.js
@@ -8,36 +8,35 @@ var child = require('child_process')
 
 var TEST_DIR = path.join(__dirname, 'test_dir')
 
-describe('DirectoryWatcher', function () {
-	
+describe('DirectoryWatcher', function() {
+
 	var watcher
 
-	beforeEach(function (done) {
+	beforeEach(function(done) {
 		rimraf(TEST_DIR, function(err) {
-			if (err) return done( err )
+			if (err) return done(err)
 
 			fs.mkdirSync(TEST_DIR)
 
 			writeTestFile('1')
 
-			setTimeout(function () {
-					
+			setTimeout(function() {
+
 				DirectoryWatcher.create(TEST_DIR, function(err, _watcher) {
 					if (err) return done(err)
 
 					watcher = _watcher
 					done()
-				})				
+				})
 			}, 500)
 		})
 	})
 
-	afterEach(function () {
+	afterEach(function() {
 		watcher.kill()
 	})
 
-	it('loads a list of files in the watched directory when it is created', 
-		function () {
+	it('loads a list of files in the watched directory when it is created', function() {
 
 		assert.ok(util.isArray(watcher.files))
 
@@ -46,26 +45,26 @@ describe('DirectoryWatcher', function () {
 		assert.strictEqual(watcher.files[0], '1')
 	})
 
-	it('emits an event when files are added', function (done) {
+	it('emits an event when files are added', function(done) {
 		var count = 0
 		var actual = []
-		
+
 		watcher.on('add', function(files) {
-			
+
 			actual = actual.concat(files)
 			if (++count === 2) {
 				assert.deepEqual(actual, ['3', '4'])
 				done()
-			}	
+			}
 		})
 
 		writeTestFile('3')
 		writeTestFile('4')
 	})
 
-	it('emits an event when files are deleted', function (done) {
-		
-		watcher.on('delete', function (files) {
+	it('emits an event when files are deleted', function(done) {
+
+		watcher.on('delete', function(files) {
 			assert.deepEqual(files, ['1'])
 			done()
 		})
@@ -74,7 +73,7 @@ describe('DirectoryWatcher', function () {
 	})
 
 	it('emits an event when files change', function(done) {
-		
+
 		watcher.on('add', function(files) {
 			assert.deepEqual(files, ['5'])
 			child.exec('echo 123 >> ' + path.join(TEST_DIR, '5'), function(err, stdout, stderr) {
@@ -90,32 +89,32 @@ describe('DirectoryWatcher', function () {
 		writeTestFile('5')
 	})
 
-	describe('works when underlying watcher does not provide filenames', function () {
-		it('for added files', function (done) {
+	describe('works when underlying watcher does not provide filenames', function() {
+		it('for added files', function(done) {
 			// reset the current files
 			watcher.files = []
 
-			watcher.on('add', function (files) {
+			watcher.on('add', function(files) {
 				assert.deepEqual(files, ['1'])
 				done()
 			})
 
 			// simulate a rename event with no files
 			watcher._onRenameEvent(null)
-		})	
+		})
 
-		it('for deleted files', function (done) {
+		it('for deleted files', function(done) {
 			// make the watcher think it had an x file
 			watcher.files.push('x')
 
-			watcher.on('delete', function (files) {
+			watcher.on('delete', function(files) {
 				assert.deepEqual(files, ['x'])
 				done()
 			})
 
 			// simulate a rename event with no files
 			watcher._onRenameEvent(null)
-		})	
+		})
 	})
 })
 
